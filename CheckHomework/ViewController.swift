106f7d99cbfd44f97c3e59940c58aa9415811f94
Nikita Kazakov <nikita.kazakov95@gmail.com>
2021-08-31 14:08:18 +0700

    Initial Commit

---
+++ CheckHomework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckHomework
+//
+//  Created by Nikita Kazakov on 31.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
